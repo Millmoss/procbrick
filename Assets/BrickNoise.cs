8dee833453e72772a2c3fb184f527a58c472b572
Millmoss <omelliff@yahoo.com>
2019-04-07 18:33:04 -0400

    Basic brick and concrete noise

--- Assets/BrickNoise.cs
+++ Assets/BrickNoise.cs
@@ -4,12 +4,50 @@ using UnityEngine;
 
 public class BrickNoise : MonoBehaviour
 {
-
+	public Material brickMat;
+	public GameObject brickMesh;
+	public int xSize;
+	public int ySize;
+	public Color brickColor;
+	public int perlinOffset;
+	public float xSplotching = 0.006f;
+	public float ySplotching = 0.006f;
+	public float xHoling = .015f;
+	public float yHoling = .015f;
+	public float holeCutoff = 0.15f;
+	public float xLining = 0.002f;
+	public float yLining = 0.006f;
+	public float lineCutoff = 0.05f;
+	//add big holes with normals
 
     void Start()
     {
-        
-    }
+		brickMesh.GetComponent<MeshRenderer>().material = brickMat;
+
+		Texture2D brickTexture = new Texture2D(xSize, ySize, TextureFormat.ARGB32, false);
+
+		for (int y = 0; y < ySize; y++)
+		{
+			for (int x = 0; x < xSize; x++)
+			{
+				float zSplotch = Mathf.PerlinNoise(x * xSplotching + perlinOffset, y * ySplotching + perlinOffset);
+				float zHole = Mathf.PerlinNoise(x * xHoling - perlinOffset, y * yHoling - perlinOffset);
+				float zLine = Mathf.PerlinNoise(x * xLining + perlinOffset, y * yLining - perlinOffset);
+				if (zHole <= holeCutoff)
+					zHole *= 1f / holeCutoff;
+				else
+					zHole = 1;
+				if (zLine > lineCutoff || zLine < 1 - lineCutoff)
+					zLine = 1f;
+				else
+					zLine /= 1f / lineCutoff * Mathf.PerlinNoise(x * 9.67f - perlinOffset, y * 8.2f + perlinOffset);
+				brickTexture.SetPixel(x, y, brickColor * zSplotch * zHole * zLine);
+			}
+		}
+		brickTexture.Apply();
+
+		brickMat.mainTexture = brickTexture;
+	}
 	
     void Update()
     {
---
+++ Assets/ConcreteNoise.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteNoise : MonoBehaviour
+{
+	public Material concreteMat;
+	public GameObject concreteMesh;
+	public int xSize;
+	public int ySize;
+	public Color concreteColor;
+	public int perlinOffset;
+	public float xSplotching = 0.006f;
+	public float ySplotching = 0.006f;
+	public float xSpotting = .015f;
+	public float ySpotting = .015f;
+	public float spotCutoff = 0.15f;
+	public float xBlotching = 0.01f;
+	public float yBlotching = 0.006f;
+
+	void Start()
+	{
+		concreteMesh.GetComponent<MeshRenderer>().material = concreteMat;
+
+		Texture2D concreteTexture = new Texture2D(xSize, ySize, TextureFormat.ARGB32, false);
+
+		for (int y = 0; y < ySize; y++)
+		{
+			for (int x = 0; x < xSize; x++)
+			{
+				float zSplotch = (Mathf.Clamp(Mathf.PerlinNoise(x * xSplotching + perlinOffset, y * ySplotching + perlinOffset), 0.3f, 1f) + .5f) / 1.5f;
+				float zSpot = Mathf.PerlinNoise(x * xSpotting - perlinOffset, y * ySpotting - perlinOffset);
+				float zBlotch = (Mathf.Clamp(Mathf.PerlinNoise(x * xBlotching - perlinOffset, y * yBlotching - perlinOffset), 0.3f, 1f) + .5f) / 1.5f;
+				if (zSpot <= spotCutoff)
+					zSpot *= 1f / spotCutoff;
+				else
+					zSpot = 1;
+				concreteTexture.SetPixel(x, y, concreteColor * zSplotch * zBlotch);
+			}
+		}
+		concreteTexture.Apply();
+
+		concreteMat.mainTexture = concreteTexture;
+	}
+
+	void Update()
+    {
+        
+    }
+}
