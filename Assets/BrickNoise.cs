c44c37bfd4d460a9995ce780890e18ee8175d00a
Millmoss <omelliff@yahoo.com>
2019-04-06 23:06:55 -0400

    Basic stuff

---
+++ Assets/BrickNoise.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickNoise : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        
+    }
+}
