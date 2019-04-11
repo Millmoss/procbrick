7da61cc6499be2faf2b3fc0863ccb61759e128b4
lessuknow <chenc18@rpi.edu>
2019-04-10 20:53:55 -0400

    Added mortar scaling.

--- Assets/CreateTexture.cs
+++ Assets/CreateTexture.cs
@@ -9,17 +9,21 @@ public class CreateTexture : MonoBehaviour
     Texture2D texture;
     public Material mat;
 
-    public int brick_count_x, brick_count_y;
+    public int num_brick_x, num_brick_y;
     public int resolution_x, resolution_y;
+    public int mortar_width, mortar_height;
     private int x_dif, y_dif;
     public BrickNoise bn;
+    private int brick_count_x, brick_count_y;
     private Point[] points;
+    private int perlin_offset = 0;
 
     // Start is called before the first frame update
     void Start()
     {
 
-
+        brick_count_x = num_brick_x;
+        brick_count_y = num_brick_y;
         x_dif = resolution_x / (brick_count_x);
         y_dif = resolution_y / (brick_count_y);
         brick_count_x++;
@@ -35,8 +39,17 @@ public class CreateTexture : MonoBehaviour
             }
         }
 
+        //First generate a blank layer of mortar.
         texture = new Texture2D(resolution_x, resolution_y);
-        
+        for(int y = 0;y  <resolution_y;y++)
+        {
+            for(int x=0;x < resolution_x;x++)
+            {
+                texture.SetPixel(x, y, Color.gray);
+                perlin_offset++;
+            }
+        }
+
         //Set positions of point's colors.
         for(int p_x = 0; p_x < brick_count_x - 0;p_x++)
             for (int p_y = 0; p_y < brick_count_y - 0; p_y++)
@@ -56,43 +69,23 @@ public class CreateTexture : MonoBehaviour
                 {
                     mid_y = (p.y + d.y) / 2;
                 }
-                /*
-                for (int x = 1; x < x_dif * 2; x++)
-                {
-                    for (int y = 1; y < y_dif; y++)
-                    {
-                        texture.SetPixel(mid_x + x - x_dif, mid_y + y - y_dif, colors[x + y * (x_dif * 2 - 1)]);
-                    }
-                }*/
 
-                
-
-                //Generate the borders.
-                for (int x = -x_dif/1 ; x < x_dif / 2 ;x++)
-                {
-                    if (mid_x + x >= 0 && mid_x + x < resolution_x)
-                        texture.SetPixel(mid_x + x, mid_y, Color.blue);
-                }
-                for (int y = -y_dif; y <  0; y++)
-                {
-                    //Make sure that the set pixel fits the bounds.
-                   if(mid_y + y >= 0 && mid_y + y < resolution_y)
-                        texture.SetPixel(mid_x, mid_y + y, Color.green);
-                }
-
+                //Generate the brick texture at each brick.
                 List<Color> colors = new List<Color>();
                 List<Color> normals = new List<Color>();
 
-                bn.GenerateTexture(x_dif - 1, y_dif - 1, ref colors, ref normals);
+                bn.GenerateTexture(x_dif - mortar_width, y_dif - mortar_height, ref colors, ref normals);
                 bn.perlinOffset++;
                 int x_count = 0, y_count = 0;
-                for(int y = -y_dif / 2 + 1; y < y_dif / 2 ; y++)
+                for(int y = (-y_dif + mortar_width) / 2; y < (y_dif - mortar_width) / 2; y++)
                 {
-                    for (int x = -x_dif/2 + 1; x < x_dif / 2; x++)
+                    for (int x = (-x_dif + mortar_height) / 2; x < (x_dif - mortar_height) / 2; x++)
                     {
+                        
                         if (p.x + x >= 0 && p.x + x < resolution_x &&
                             p.y + y >= 0 && p.y + y < resolution_y)
-                            texture.SetPixel(p.x + x , p.y + y, colors[x_count + y_count * (x_dif - 1)]);
+                            //texture.SetPixel(p.x + x , p.y + y, Color.magenta);
+                            texture.SetPixel(p.x + x , p.y + y, colors[x_count + y_count * (x_dif - mortar_width / 1)]);
                         x_count++;
                     }
                     y_count++;
@@ -106,7 +99,7 @@ public class CreateTexture : MonoBehaviour
             for (int p_y = 0; p_y < brick_count_y; p_y++)
             {
                 Point p = getPoint(p_x,p_y);
-                texture.SetPixel(p.x, p.y, Color.red);
+                //texture.SetPixel(p.x, p.y, Color.red);
             }
 
         texture.Apply();
@@ -119,5 +112,11 @@ public class CreateTexture : MonoBehaviour
         if (x >= brick_count_x || y >= brick_count_y)
             return null;
         return points[x + y * brick_count_x];
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+            Start();
     }
 }
